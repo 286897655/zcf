a1572c2e44ab7b0e06f3c307715af68a1079b319
zhaoj361 <zhaoj361@chainaunicom.cn>
2024-02-28 14:53:32 +0800

    improve

---
+++ include/zcf/zcf_buffer.hpp
@@ -0,0 +1,13 @@
+#ifndef ZCF_BUFFER_HPP_
+#define ZCF_BUFFER_HPP_
+
+namespace zcf{
+
+class bit_buffer{
+
+};
+
+}//!namespace zcf
+
+
+#endif //!ZCF_BUFFER_HPP_
---
+++ src/zcf_utility.cpp
@@ -0,0 +1,7 @@
+#include <zcf/zcf_utility.hpp>
+
+namespace zcf{
+
+    // zcf library version definition
+    extern const char kZCFVersion[] = "0.0.1";
+}//!namespace zcf
